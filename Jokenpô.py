844aec88ec77056324abf42f365dc2a5eb5727b6
AndressaCamillo85 <estudosdanegah@gmail.com>
2024-08-04 21:59:53 -0300

    Create Jokenpô.py

---
+++ Jokenpô.py
@@ -0,0 +1,23 @@
+#Jogo Jokenpô
+let Pedra = número = 1
+let Papel = número = 2
+let Tesoura = número = 3
+
+let jogador1 = número
+let jogador2 = número
+
+Jogador1 (int(input('Qual é a sua jogada? \n Digite um número')))
+número = float (input)
+print (número)
+
+Jogador2 (int(input('Qual é a sua jogada? \n Digite um número')))
+número = flat (input)
+print (número)
+
+if(jogador1 == número = 2 and jogador2 == número 1; or jogador1 == número = 1 and jogador2 == número = 3; or jogador1 == número = 3 and jogador2 == número 2):
+    print ('Jogador 1: Você venceu')
+elseif(jogador2 == número = 2 and jogador1 == número 1; or jogador2 == número = 1 and jogador1 == número = 3; or jogador2 == número = 3 and jogador1 == número 2):
+    print('Jogador 2: Você venceu')
+else(jogador1 == número jogador2)
+    print ('Empate')
+
