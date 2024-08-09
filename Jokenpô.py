370f9eb7a2d91dca88c66865f67d9100799bd1a0
AndressaCamillo85 <estudosdanegah@gmail.com>
2024-08-09 11:19:47 -0300

    Update Jokenpô.py

--- Jokenpô.py
+++ Jokenpô.py
@@ -1,23 +1,28 @@
 #Jogo Jokenpô
-let Pedra = número = 1
-let Papel = número = 2
-let Tesoura = número = 3
 
-let jogador1 = número
-let jogador2 = número
+jogador1 = pedra, papel, tesoura
+jogador2 = pedra, papel, tesoura
 
-Jogador1 (int(input('Qual é a sua jogada? \n Digite um número')))
-número = float (input)
-print (número)
+jogador1 (input('Digite pedra, papel, tesoura.\n Qual é a sua jogada? '))
+jogador2 (input('Digite pedra, papel, tesoura.\n Qual é a sua jogada? '))
 
-Jogador2 (int(input('Qual é a sua jogada? \n Digite um número')))
-número = float (input)
-print (número)
-
-if(jogador1 == número = 2 and jogador2 == número 1; or jogador1 == número = 1 and jogador2 == número = 3; or jogador1 == número = 3 and jogador2 == número 2):
-    print ('Jogador 1: Você venceu')
-elseif(jogador2 == número = 2 and jogador1 == número 1; or jogador2 == número = 1 and jogador1 == número = 3; or jogador2 == número = 3 and jogador1 == número 2):
-    print('Jogador 2: Você venceu')
-else(jogador1 == número jogador2)
-    print ('Empate')
-
+if(jogador1 == "papel"):
+       elif(jogador2 == "pedra"):
+           print ('Jogador 1: Você venceu')
+           elif(jogador1 == "pedra"):
+               elif(jogador2 == "tesoura"):
+                   print ('Jogador 1: Você venceu')
+                   elif(jogador1 == "tesoura"):
+                       elif(jogador2 == "papel"):
+                           print('Jogador 1: Você venceu')
+                           elif(jogador2 == "papel"):
+                               elif(jogador1 == "pedra"):
+                                   print ('Jogador 2: Você venceu')
+                                       elif(jogador2 == "pedra"):
+                                           elif(jogador1 == "tesoura"):
+                                               print ('Jogador 2: Você venceu')
+                                                   elif(jogador2 == "tesoura"):
+                                                       elif(jogador1 == "papel"):
+                                                           print('Jogador 2: Você venceu')
+                                                           else(jogador 1 == jogador 2):
+print ('Empate')
