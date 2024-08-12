e91b52559cc8e6d3aaca4f18b2916b55410d7457
Programação em Python 2024.4 <36129532024.4@senacrjedu.local>
2024-08-12 09:54:52 -0300

    add aula sexta

---
+++ lista.py
@@ -0,0 +1,21 @@
+doces = [ ]
+
+doces.append('Pudim')
+doces.append('Brigadeiro')
+doces.append('Pave')
+doces.append('Quindim')
+doces.append('Torta de Limao')
+
+'''doces.insert(3, 'Cajuzinho')
+print(doces)'''
+
+doces.sort()
+print(doces)
+
+'''print ('Imprimir lista')
+print(doces)'''
+
+
+
+'''for doce in doces:
+    print(doce)'''
