27389c47859471efd953a4aff68348bb2b6236e5
AndressaCamillo85 <estudosdanegah@gmail.com>
2024-08-16 12:46:02 -0300

    Create 12.py

---
+++ 12.py
@@ -0,0 +1,11 @@
+'''O usuário irá digitar a sua idade e, caso a idade seja maior ou igual a 18, terá como resposta “Maior de Idade”. Caso contrário, “Menor de Idade”.'''
+
+MaiorIdade = 18
+
+idade = int(input("Digite a sua idade:\n"))
+
+if (idade >= MaiorIdade):
+    print("Maior de Idade")
+else:
+        print("Menor de Idade")
+        
